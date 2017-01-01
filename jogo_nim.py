139b9b7a0f374aa9746c44019b64b73ecd7d8c8f
GesrobDR <david81br@gmail.com>
2017-01-01 19:58:31 -0300

    Função campeonato modificada

--- jogo_nim.py
+++ jogo_nim.py
@@ -1,33 +1,50 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-rodada=0 	# - Conta o número de rodadas.
-n = 0 		# - Registra o número de peças.
-m = 0		# - Registra o limite de peças por jogada.
+#rodada=1	# - Conta o número de rodadas.
+#n = 0 		# - Registra o número de peças.
+#m = 0		# - Registra o limite de peças por jogada.
 
 def computador_escolhe_jogada(n, m):
-	return 
+    while True:
+        if (n-m) % (m+1) == 0:
+            return m
+        else:
+            m=m-1
+            return m
+
 
 def usuario_escolhe_jogada(n,m):
-
-	while True:
-		m_user=int(input("Quantas peças você vai tirar"))
-		print(m_user)
-		if m_user>m:
-			print("Oops! Jogada inválida! Tente de novo.")
-		else:
-			return int(m_user)
+    while True:
+        m_user=int(input("Quantas peças você vai tirar"))
+	print(m_user)
+	if m_user>m:
+	    print("Oops! Jogada inválida! Tente de novo.")
+	else:
+            break
+    return int(m_user)
 
 
 def campeonato():
-	pass
+    rodada=1
+    print("Quantas peças?")
+    n = int(input())
+    print("Limite de peças por jogada? ")
+    m = int(input())
+    while rodada<=3:
+        if (n-m) % (m+1) == 0:
+            print("Computador começa!")
+        else:
+            print("Você começa!")
+        rodada+=1
 
 def partida():
-	print("Quantas peças?",end=" ")
+
+	print("Quantas peças?")
 	n = int(input())
-	print("Limite de peças por jogada?", end=" ")
+	print("Limite de peças por jogada? ")
 	m = int(input())
-	if n % (m+1) == 0:
+	if (n-m) % (m+1) == 0:
 		f=usuario_escolhe_jogada(n,m)
 		n=n-f
 	else:
@@ -39,13 +56,14 @@ def partida():
 
 
 def main():
-	print("Bem-vind ao jogo do NIM! Escolha:")
+	print("Bem-vindo ao jogo do NIM! Escolha:")
 	print("1 - para jogar uma partida isolada")
 	print("2 - para jogar um campeonato", end=" ")
 	choicy = input()
 
 	if choicy=='2':
 		print("Você escolheu um campeonato!")
+                campeonato()
 	elif choicy=='1':
 		print("Você escolheu uma partida apenas.")
 		partida()
